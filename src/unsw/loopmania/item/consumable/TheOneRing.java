934dce7d1452c5c5cb4a1dab46f56e21d34773a2
sunny <coolcanman987@gmail.com>
2021-07-16 21:36:23 +1000

    implement basic consumable entities

---
+++ src/unsw/loopmania/item/consumable/TheOneRing.java
@@ -0,0 +1,15 @@
+package unsw.loopmania.item.consumable;
+
+import unsw.loopmania.Character;
+
+public class TheOneRing extends Consumable {
+    private static final int FULL_HEALTH = 100;
+    public TheOneRing(SimpleIntegerProperty x, SimpleIntegerProperty y) {
+        super(x, y);
+    }
+
+    @Override
+    public void consume(Character character) {
+        character.setHealth(FULL_HEALTH);        
+    }
+}
