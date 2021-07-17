a3be57913a92e865104fb00431b68d0fc9d5bcb0
KayoshT <kaythiyaga@gmail.com>
2021-07-17 17:03:13 +1000

    commit placable path

--- src/unsw/loopmania/Cards/PlacableBehaviour/PlacableOnNonPath.java
+++ src/unsw/loopmania/Cards/PlacableBehaviour/PlacableOnNonPath.java
@@ -4,8 +4,7 @@ public class PlacableOnNonPath implements PlacableBehaviour{
 
     @Override
     public void placable() {
-        // TODO Auto-generated method stub
-        
+            
     }
     
 }
