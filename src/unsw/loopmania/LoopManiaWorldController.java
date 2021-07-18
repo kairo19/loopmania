41c747f18ed6087ee1a3ae9bb9fe1989fd3ec2e0
Sunny <sunnyw987@gmail.com>
2021-07-19 07:01:35 +1000

    quickfix

--- src/unsw/loopmania/LoopManiaWorldController.java
+++ src/unsw/loopmania/LoopManiaWorldController.java
@@ -35,6 +35,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
@@ -331,6 +332,9 @@ public class LoopManiaWorldController {
         isPaused = false;
         // trigger adding code to process main game logic to queue. JavaFX will target framerate of 0.3 seconds
         timeline = new Timeline(new KeyFrame(Duration.seconds(0.3), event -> {
+            if (world.isGameover() || world.getCharacterHealthProperty().get() <= 0) {
+                gameOver(world.hasMetGoal());
+            }
             if (true /*charcter is at heroscastle */) {
                 openStore();
             }
