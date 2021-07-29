87f86faaf7089a5a7dfadb1610cb78087ec0ed40
Sunny <sunnyw987@gmail.com>
2021-07-29 17:58:29 +1000

    add shopview and controller skeleton

---
+++ src/unsw/loopmania/ShopController.java
@@ -0,0 +1,17 @@
+package unsw.loopmania;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ShopController {
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    void handleReturnButton(ActionEvent event) {
+        
+    }
+
+}
