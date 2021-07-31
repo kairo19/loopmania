b5d2c6d2bfb23bf7f8d057a9a73ddc0ad75f13ab
Sunny <sunnyw987@gmail.com>
2021-08-01 01:11:53 +1000

    implement shop sell controller skeleton

---
+++ src/unsw/loopmania/ShopSellController.java
@@ -0,0 +1,74 @@
+package unsw.loopmania;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class ShopSellController {
+    @FXML
+    private Text nSwords;
+
+    @FXML
+    private Text nStakes;
+
+    @FXML
+    private Text nStaffs;
+
+    @FXML
+    private Text nArmours;
+
+    @FXML
+    private Text nShields;
+
+    @FXML
+    private Text nHelmets;
+
+    @FXML
+    private Text nPotions;
+
+    @FXML
+    void returnToLMA(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnToShop(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sellArmour(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sellHelmet(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sellPotion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sellShield(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sellStaff(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sellStake(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sellSword(ActionEvent event) {
+
+    }
+
+}
