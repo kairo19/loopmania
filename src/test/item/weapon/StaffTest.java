ebf70929d9ead38ae4f664346e6bab303db8aee7
GilbertCodes <68584921+GilbertCodes@users.noreply.github.com>
2021-08-02 02:08:18 +1000

    staff dospecial test

--- src/test/item/weapon/StaffTest.java
+++ src/test/item/weapon/StaffTest.java
@@ -34,13 +34,40 @@ public class StaffTest {
         d.setCharacter(character);
         Staff staff = new Staff(new SimpleIntegerProperty(), new SimpleIntegerProperty());
         BasicEnemy slug = new Slug(new PathPosition(0, orderedpath));
+
         character.setWeapon(staff);
         character.dealDamage(slug, 0);
         assertEquals(3, slug.getHealth()); 
     }
 
     @Test
-    public void testStaffSpecial() {
+    public void testStaffdoSpecial() {
+        List<Pair<Integer, Integer>> orderedpath  = new ArrayList<>();
+        orderedpath.add(new Pair<Integer, Integer>(1, 1));
+        orderedpath.add(new Pair<Integer, Integer>(2, 1));
+        orderedpath.add(new Pair<Integer, Integer>(3, 1));
+        orderedpath.add(new Pair<Integer, Integer>(3, 2));
+        orderedpath.add(new Pair<Integer, Integer>(3, 3));
+        orderedpath.add(new Pair<Integer, Integer>(2, 3));
+        orderedpath.add(new Pair<Integer, Integer>(1, 3));
+        orderedpath.add(new Pair<Integer, Integer>(1, 2));
+        LoopManiaWorld d = new LoopManiaWorld(4, 4, orderedpath);
+        Character character = new Character(new PathPosition(0, orderedpath));
+        d.setCharacter(character);
+        Staff staff = new Staff(new SimpleIntegerProperty(), new SimpleIntegerProperty());
+        BasicEnemy slug = new Slug(new PathPosition(0, orderedpath));
+        slug.setHealth(700);
+        for (int tester = 0; tester < 30; tester++) {
+            
+            character.setWeapon(staff);
+            character.dealDamage(slug, 0);
+        }
+        boolean tranceOccured = false;
+        if (character.getAllies() > 0) {
+            tranceOccured = true;
+        }
+        assertEquals(490, slug.getHealth());
+        assertEquals(true, tranceOccured);
         
     }
 }
