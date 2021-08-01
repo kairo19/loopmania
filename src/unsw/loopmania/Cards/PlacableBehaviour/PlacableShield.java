12973cc7c1bb58c180a4cc1f5bd92e10ecead1a5
William Huang <weida.huang@student.unsw.edu.au>
2021-08-01 22:37:39 +1000

    Update PlacableShield.java

--- src/unsw/loopmania/Cards/PlacableBehaviour/PlacableShield.java
+++ src/unsw/loopmania/Cards/PlacableBehaviour/PlacableShield.java
@@ -3,17 +3,30 @@ package unsw.loopmania.Cards.PlacableBehaviour;
 import java.util.List;
 import org.javatuples.Pair;
 
+/**
+ * This class represents the shield card placeable behaviour.
+ */
+
 public class PlacableShield implements PlacableBehaviour{
 
+    /**
+     * Returns whether the item is placeable.
+     * @param x - card x coord position.
+     * @param y - card y coord position.
+     * @param orderedPath - all path coordinates.
+     */
     @Override
     public boolean placable(int x, int y, List<Pair<Integer, Integer>> orderedPath) {
-        // TODO Auto-generated method stub
         return false;
     }
 
+    /**
+     * Implements logic for placing.
+     * @param x - game world x coord position to place.
+     * @param y - game world y coord position to place.
+     */
     @Override
     public boolean itemPlacable(int x, int y) {
-        // TODO Auto-generated method stub
         if (x == 2 && y == 1) {
             return true;
         } else {
