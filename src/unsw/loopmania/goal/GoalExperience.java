99f570c12d69cc7454f6ce652f95bf09fd98f6d7
William Huang <weida.huang@student.unsw.edu.au>
2021-08-01 20:50:59 +1000

    Update GoalExperience.java

--- src/unsw/loopmania/goal/GoalExperience.java
+++ src/unsw/loopmania/goal/GoalExperience.java
@@ -2,6 +2,12 @@ package unsw.loopmania.goal;
 
 import unsw.loopmania.LoopManiaWorld;
 
+/**
+ * This class represents goal type with experience requirements.
+ * @param loopManiaWorld - world state.
+ * @param quantity - xp points for goal criteria.
+ */
+
 public class GoalExperience implements GoalNode {
     private LoopManiaWorld loopManiaWorld;
     private int experienceGoal;
@@ -10,21 +16,35 @@ public class GoalExperience implements GoalNode {
         this.experienceGoal = quantity;
     }
 
+    /**
+     * Returns whether goals have been met.
+     * @param loopManiaWorld - world state.
+     */
     @Override
     public boolean hasMetGoal(LoopManiaWorld loopManiaWorld) {
         return loopManiaWorld.getExperience() >= experienceGoal;
     }
 
+    /**
+     * Returns whether goal is leaf node.
+     */
     @Override
     public boolean isLeafNode() {
         return true;
     }
 
+    /**
+     * Adds a sub-goal
+     * @param subGoal - a sub-goal to add to AND conditions.
+     */
     @Override
     public void addSubGoal(GoalNode subGoal) {
         // do nothing
     }
 
+    /**
+     * Returns goal condition as concatenated string.
+     */
     @Override
     public String toString() {
         String s = "experience : " + experienceGoal;
