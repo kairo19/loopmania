2bc47fdf0c560238070a644b13b609f78945fed0
William Huang <weida.huang@student.unsw.edu.au>
2021-08-01 22:05:09 +1000

    Update Helmet.java

--- src/unsw/loopmania/item/defensiveitem/Helmet.java
+++ src/unsw/loopmania/item/defensiveitem/Helmet.java
@@ -6,6 +6,12 @@ import unsw.loopmania.Enemies.BasicEnemy;
 import unsw.loopmania.StaticEntity;
 import unsw.loopmania.Cards.PlacableBehaviour.PlacableHelmet;
 
+/**
+ * This class represents the helmet defensive item which reduces damage taken from enemies but debuffs character attack.
+ * @param x - item x coord position.
+ * @param y - item y coord position.
+ */
+
 public class Helmet extends DefensiveItem implements Blinder {
     private static final double DAMAGE_REDUCTION = 5.0;
     private static final int DEBUFF_VALUE = 5;
@@ -14,11 +20,17 @@ public class Helmet extends DefensiveItem implements Blinder {
         this.placableBehaviour = new PlacableHelmet();
     }
 
+    /**
+     * Returns the integer reduction to character attack caused by the blinding debuff.
+     */
     @Override
     public int debuff() {
         return DEBUFF_VALUE;
     }
 
+    /**
+     * Returns item name as string.
+     */
     @Override
     public String toString() {
         // TODO Auto-generated method stub
