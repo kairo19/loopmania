512e29bc2f7e3f14cdfe4171efb964e9817206a9
William Huang <weida.huang@student.unsw.edu.au>
2021-08-01 22:24:21 +1000

    Update VillageCard.java

--- src/unsw/loopmania/Cards/VillageCard.java
+++ src/unsw/loopmania/Cards/VillageCard.java
@@ -7,11 +7,21 @@ import javafx.util.Pair;
 import unsw.loopmania.Cards.PlacableBehaviour.PlacableOnNonPath;
 import unsw.loopmania.Cards.PlacableBehaviour.PlacableOnPath;
 
+/**
+ * This class represents the village building card.
+ * @param x - building card x coord position.
+ * @param y - building card y coord position.
+ */
+
 public class VillageCard extends Card {
     public VillageCard(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y);
         this.placableBehaviour = new PlacableOnPath();
     }   
+
+    /**
+     * Returns card name as string.
+     */
     @Override
     public String toString() {
         return "VillageCard";
