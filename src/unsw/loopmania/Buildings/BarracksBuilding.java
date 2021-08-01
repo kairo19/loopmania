f7e954ea3237d2b3f781504b27c9ff9039f57033
William Huang <weida.huang@student.unsw.edu.au>
2021-08-01 19:33:20 +1000

    Update BarracksBuilding.java

--- src/unsw/loopmania/Buildings/BarracksBuilding.java
+++ src/unsw/loopmania/Buildings/BarracksBuilding.java
@@ -7,11 +7,18 @@ import org.javatuples.Pair;
 import javafx.beans.property.SimpleIntegerProperty;
 import unsw.loopmania.Character;
 
+/**
+ * This class represents the barracks building.
+ * @param x - building x coord position.
+ * @param y - building y coord position.
+ */
+
 public class BarracksBuilding extends Building {
 
     public BarracksBuilding(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y);
     }
+    
     /**
      * Adds an extra ally to the character object
      * @param character
@@ -20,6 +27,10 @@ public class BarracksBuilding extends Building {
     public void CharacterBuffAbility(Character character) {
         character.gainAlly();
     }
+    
+    /**
+     * Returns building name as string.
+     */
     @Override
     public String toString() {
         
