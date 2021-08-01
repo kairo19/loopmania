9a8ca9718a84fc41bd07c547709d6c1ac410fd4c
William Huang <weida.huang@student.unsw.edu.au>
2021-08-01 20:36:43 +1000

    Update Zombie.java

--- src/unsw/loopmania/Enemies/Zombie.java
+++ src/unsw/loopmania/Enemies/Zombie.java
@@ -5,19 +5,31 @@ import java.util.Random;
 import unsw.loopmania.PathPosition;
 import unsw.loopmania.Character;
 
+/**
+ * This class represents a zombie enemy.
+ * @param pathPosition - enemy position on path.
+ */
+
 public class Zombie extends BasicEnemy implements SpecialAbility {
     public Zombie(PathPosition pathPosition) {
-        // Gives 2 gold and 2 xp
         // 2 battle radius, 2 support radius (nullifies since == to battle radius)
         super(pathPosition, 25, 10, "Zombie", 4, 4, 2, 2);
     }
 
+    /**
+     * Zombie deals damage to character.
+     * @param character - character entity.  
+     */
     @Override
     public void dealDamage(Character character) {
         super.dealDamage(character);
 
     }
 
+    /**
+     * Zombie special attack which kills a character ally.
+     * @param character - character entity.  
+     */
     @Override
     public boolean doSpecial(Character character) {
         Random r = new Random();
@@ -33,6 +45,9 @@ public class Zombie extends BasicEnemy implements SpecialAbility {
         return false;
     }
 
+    /**
+     * Returns enemy name as string.
+     */
     @Override
     public String toString() {
         return "Zombie";
