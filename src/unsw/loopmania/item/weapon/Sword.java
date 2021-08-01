08ee64068e373b068a8730611a695849ef1af977
William Huang <weida.huang@student.unsw.edu.au>
2021-08-01 21:18:36 +1000

    Update Sword.java

--- src/unsw/loopmania/item/weapon/Sword.java
+++ src/unsw/loopmania/item/weapon/Sword.java
@@ -5,9 +5,13 @@ import unsw.loopmania.Character;
 import unsw.loopmania.StaticEntity;
 import unsw.loopmania.Enemies.BasicEnemy;
 import unsw.loopmania.Cards.PlacableBehaviour.PlacableWeapon;
+
 /**
- * represents an equipped or unequipped sword in the backend world
+ * This class represents the sword weapon item which boosts attack by 15 damage.
+ * @param x - item x coord position.
+ * @param y - item y coord position.
  */
+
 public class Sword extends StaticEntity implements Weapon {
     private static final int DAMAGE_BOOST = 15;  
     public Sword(SimpleIntegerProperty x, SimpleIntegerProperty y) {
@@ -15,25 +19,39 @@ public class Sword extends StaticEntity implements Weapon {
         this.placableBehaviour = new PlacableWeapon();
     }
 
+    /**
+     * Applies damage boost to character.
+     * @param character - character entity.
+     */
     @Override
     public void damageBoost(Character character) {
         //character.setDamage(character.getDamage() + DAMAGE_BOOST);
         character.setBuffedDamage(DAMAGE_BOOST);
     }
 
+    /**
+     * Applies item special attack to an enemy.
+     * @param character - character entity.
+     * @param basicEnemy - enemy entity.
+     */
     @Override
     public void doSpecial(BasicEnemy basicEnemy, Character character) {
         // do nothing - sword not special :(
     }
 
+    /**
+     * Checks whether item is placable.
+     */
     public boolean checkItemplacable(int x, int y){
         
         return placableBehaviour.itemPlacable(x, y);
     }
-    
+
+    /**
+     * Returns item name as string.
+     */
     @Override
     public String toString() {
-        // TODO Auto-generated method stub
         return "Sword";
     }
 }
