83e18f48d3d7bae0a7652389ce44045944c7a24c
William Huang <weida.huang@student.unsw.edu.au>
2021-08-01 22:31:34 +1000

    Update PlacableBehaviour.java

--- src/unsw/loopmania/Cards/PlacableBehaviour/PlacableBehaviour.java
+++ src/unsw/loopmania/Cards/PlacableBehaviour/PlacableBehaviour.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.javatuples.Pair;
 
 
+/**
+ * Interface for behaviour when placing item cards.
+ */
 
 public interface PlacableBehaviour {
     public boolean placable(int x, int y, List<Pair<Integer, Integer>> orderedPath);
