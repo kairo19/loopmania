91967753617b9992bb98be8f8a9f44a8ab6a44f6
William Huang <weida.huang@student.unsw.edu.au>
2021-08-01 22:16:23 +1000

    Update BarracksCard.java

--- src/unsw/loopmania/Cards/BarracksCard.java
+++ src/unsw/loopmania/Cards/BarracksCard.java
@@ -5,12 +5,22 @@ import java.util.List;
 import javafx.beans.property.SimpleIntegerProperty;
 import unsw.loopmania.Cards.PlacableBehaviour.PlacableOnPath;
 
+/**
+ * This class represents the barracks building card.
+ * @param x - building card x coord position.
+ * @param y - building card y coord position.
+ */
+
 public class BarracksCard extends Card {
 
     public BarracksCard(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y);
         placableBehaviour = new PlacableOnPath();
     }  
+
+    /**
+     * Returns card name as string.
+     */
     @Override
     public String toString() {
         return "BarracksCard";
