93bea1b5f0c5cfca6d5a3cf3d613648efcb378b8
William Huang <weida.huang@student.unsw.edu.au>
2021-08-01 21:19:10 +1000

    Update Weapon.java

--- src/unsw/loopmania/item/weapon/Weapon.java
+++ src/unsw/loopmania/item/weapon/Weapon.java
@@ -3,6 +3,10 @@ package unsw.loopmania.item.weapon;
 import unsw.loopmania.Enemies.BasicEnemy;
 import unsw.loopmania.Character;
 
+/**
+ * Interface for weapon items.
+ */
+
 public interface Weapon {
     public void damageBoost(Character character);
     public void doSpecial(BasicEnemy basicEnemy, Character character); // can be replaced with a gettype() method?
