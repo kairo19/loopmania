1d56b6c1e5c0fc3a45daf51660155560167c95b5
William Huang <weida.huang@student.unsw.edu.au>
2021-08-01 20:30:39 +1000

    Update SpecialAbility.java

--- src/unsw/loopmania/Enemies/SpecialAbility.java
+++ src/unsw/loopmania/Enemies/SpecialAbility.java
@@ -1,6 +1,11 @@
 package unsw.loopmania.Enemies;
 import unsw.loopmania.Character;
 
+/**
+ * Interface for enemy special attack abilities.
+ * @param character - character entity.
+ */
+
 public interface SpecialAbility {
     public boolean doSpecial(Character character);
 }
